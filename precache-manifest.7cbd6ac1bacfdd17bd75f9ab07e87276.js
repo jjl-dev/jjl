b171bcd29904bc35c5fccb37dc1ab8bda7cfeed0
Chris Girard <chris@routifc.com>
2019-06-11 21:16:06 -0700

    Updates

---
+++ precache-manifest.7cbd6ac1bacfdd17bd75f9ab07e87276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71b3a6f670c834262c28903950900a7",
+    "url": "/jjl/index.html"
+  },
+  {
+    "revision": "61d7ccc0c5217408ae31",
+    "url": "/jjl/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/jjl/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "61d7ccc0c5217408ae31",
+    "url": "/jjl/static/js/main.be315aa7.chunk.js"
+  },
+  {
+    "revision": "c09b1590abae3d6586b4",
+    "url": "/jjl/static/js/runtime~main.5e541059.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jjl/static/media/logo.5d5d9eef.svg"
+  }
+]);
