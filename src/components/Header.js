5bdab3e2689729349c55453fb993894c02acce3d
Chris Girard <chris@routifc.com>
2019-09-22 19:02:31 -0700

    adding mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  NavLink
-} from "react-router-dom";
-import styled from "styled-components";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
 import { Media } from "react-breakpoints";
 
 import logo from "../assets/jjl-logo.svg";
 import logoCompact from "../assets/jjl-logo-compact.svg";
 
+const GlobalNavStyle = createGlobalStyle`
+  body {
+    background: ${props => "#E8E6DC"};
+  }
+  header{
+    background: ${props => "#E8E6DC !important"};
+  }
+  hr {
+    border-top: 1px solid #C8C8C8; 
+  }
+`;
 
 const Container = styled.header`
   padding: 20px 15px;
@@ -34,48 +42,184 @@ const Links = styled.div`
   display: flex;
   justify-content: space-between;
   flex-grow: 1;
+
+  @media only screen and (max-width: 575px) {
+    display: block;
+    font-size: 16px;
+    line-height: 27px;
+    padding: 34px 14px;
+  }
 `;
 
-const Header = () => {
-  return (
-    <React.Fragment>
-      <Container>
-        <Logo>
-          <NavLink activeClassName="is-active" to="/projects">
-            <Media>
-              {({ breakpoints, currentBreakpoint }) =>
-                breakpoints[currentBreakpoint] > breakpoints.mobile ? (
-                  <img src={logo} width="126" alt="jjl logo" />
+const MobileNavIcon = styled.div`
+  width: 14px;
+  height: 14px;
+  border-radius: 14px;
+  border: 1px solid #3f3b33;
+`;
+
+const MobileNavIconActive = styled.div`
+  width: 14px;
+  height: 14px;
+  border-radius: 14px;
+  background: #3f3b33;
+  border: 1px solid #3f3b33;
+`;
+
+const MobileNav = styled.div`
+  height: 100vh;
+  position: absolute;
+  background: #e8e6dc;
+  width: 100%;
+  top: 56px;
+  right: 0;
+  border-top: 1px solid #c8c8c8;
+  -webkit-animation: fadein .5s; /* Safari, Chrome and Opera > 12.1 */
+  -moz-animation: fadein .5s; /* Firefox < 16 */
+  -ms-animation: fadein .5s; /* Internet Explorer */
+  -o-animation: fadein .5s; /* Opera < 12.1 */
+  animation: fadein .5s;
+`;
+
+const Bio = styled.div`
+  padding: 50px 14px;
+  font-size: 15px;
+  line-height: 21px;
+  font-family: Suisse Works Intl;
+  position: absolute;
+  bottom: 40px;
+`;
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+  }
+
+  handleMenuClick() {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  }
+
+  handleLinkClick() {
+    this.setState({ menuOpen: false });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Media>
+          {({ breakpoints, currentBreakpoint }) =>
+            breakpoints[currentBreakpoint] > breakpoints.mobile ? (
+              <Container>
+                <Logo>
+                  <NavLink activeClassName="is-active" to="/projects">
+                    <img src={logo} width="126" alt="jjl logo" />
+                  </NavLink>
+                </Logo>
+                <Links>
+                  <div>
+                    <NavLink activeClassName="is-active" to="/projects">
+                      PROJECTS
+                    </NavLink>
+                    /
+                    <NavLink activeClassName="is-active" to="/overview">
+                      OVERVIEW
+                    </NavLink>
+                  </div>
+                  <div>
+                    <NavLink activeClassName="is-active" to="/about">
+                      ABOUT
+                    </NavLink>
+                    /
+                    <NavLink activeClassName="is-active" to="/Journal">
+                      JOURNAL
+                    </NavLink>
+                  </div>
+                </Links>
+              </Container>
+            ) : (
+              <Container>
+                <Logo>
+                  <NavLink activeClassName="is-active" to="/projects" onClick={() => this.handleLinkClick()}>
+                    <img src={logoCompact} width="40" alt="jjl logo" />
+                  </NavLink>
+                </Logo>
+                {this.state.menuOpen ? (
+                  <MobileNavIconActive
+                    onClick={() => this.handleMenuClick()}
+                  ></MobileNavIconActive>
                 ) : (
-                  <img src={logoCompact} width="40" alt="jjl logo" />
-                )
-              }
-            </Media>
-          </NavLink>
-        </Logo>
-        <Links>
-          <div>
-            <NavLink activeClassName="is-active" to="/projects">
-              PROJECTS
-            </NavLink>
-            /
-            <NavLink activeClassName="is-active" to="/overview">
-              OVERVIEW
-            </NavLink>
-          </div>
-          <div>
-            <NavLink activeClassName="is-active" to="/about">
-              ABOUT
-            </NavLink>
-            /
-            <NavLink activeClassName="is-active" to="/Journal">
-              JOURNAL
-            </NavLink>
-          </div>
-        </Links>
-      </Container>
-    </React.Fragment>
-  );
-};
+                  <MobileNavIcon
+                    onClick={() => this.handleMenuClick()}
+                  ></MobileNavIcon>
+                )}
+
+                {this.state.menuOpen && (
+                  <React.Fragment>
+                    <GlobalNavStyle />
+                    <MobileNav className={this.state.menuOpen ? 'hello' : 'nope'}>
+                      <Links>
+                        <div>
+                          <NavLink activeClassName="is-active" to="/projects" onClick={() => this.handleLinkClick()}>
+                            PROJECTS
+                          </NavLink>
+                          /
+                          <NavLink activeClassName="is-active" to="/overview" onClick={() => this.handleLinkClick()}>
+                            OVERVIEW
+                          </NavLink>
+                        </div>
+                        <div>
+                          <NavLink activeClassName="is-active" to="/about" onClick={() => this.handleLinkClick()}>
+                            ABOUT
+                          </NavLink>
+                          /
+                          <NavLink activeClassName="is-active" to="/Journal" onClick={() => this.handleLinkClick()}>
+                            JOURNAL
+                          </NavLink>
+                        </div>
+                      </Links>
+                      <Bio>
+                        <p>
+                          Jeremy Jude Lee is a lifestyle and commercial
+                          photographer and videographer from Vancouver, Canada.
+                          He started photography as a teenager, when he picked
+                          up his parents’ cameras, and started recording
+                          whatever was of interest around him.
+                          <br />
+                          Now, almost a decade later, Jeremy has solidified a
+                          strong reputation as one of Vancouver’s “go to”
+                          photographers, telling visual stories for a long list
+                          of a long list of editorial and commercial clients.
+                        </p>
+                        <br />
+                        <a
+                          href="https://www.instagram.com/jeremyjudelee"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Instagram 
+                        </a> 
+                        / 
+                        <a
+                          href="https://www.instagram.com/jeremyjudelee"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Email
+                        </a>
+                      </Bio>
+                    </MobileNav>
+                  </React.Fragment>
+                )}
+              </Container>
+            )
+          }
+        </Media>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Header;
