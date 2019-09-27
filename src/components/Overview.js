c0bf3ba62dcf6d3cfc695265b74eb131d9a4c152
Chris Girard <chris@routifc.com>
2019-09-26 19:21:40 -0700

    adding overlay image fadein

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
+import FadeIn from "react-lazyload-fadein";
 
 import img1 from "../assets/OVERVIEW-jeremyjudelee-vancouver-lifestyle-commercial-photographer-1.jpg";
 import img2 from "../assets/OVERVIEW-jeremyjudelee-vancouver-lifestyle-commercial-photographer-2.jpg";
@@ -113,173 +114,391 @@ class Overview extends React.Component {
       this.setState({ photoIndex: e - 1 });
     };
 
+    const imageFadeInDuration = 400;
+    const imageHeight = 700;
+
     return (
       <React.Fragment>
         <Container>
           <ImgContainer>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(1)}
-                src={img1}
-                width="237"
-                alt="jjl img1"
-              />
-              <Img
-                onClick={() => handlePhotoClick(2)}
-                src={img2}
-                width="192"
-                alt="jjl img2"
-              />
-              <Img
-                onClick={() => handlePhotoClick(3)}
-                src={img3}
-                width="432"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(1)}
+                    src={img1}
+                    onLoad={onload}
+                    width="237"
+                    alt={img1}
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(2)}
+                    src={img2}
+                    onLoad={onload}
+                    width="192"
+                    alt={img2}
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(3)}
+                    src={img3}
+                    onLoad={onload}
+                    width="432"
+                    alt={img3}
+                  />
+                )}
+              </FadeIn>
+            </ImgRow>
+
+            <ImgRow>
+              <FadeIn height={imageHeight} duration={imageFadeInDuration} easing={"ease-in-out"}>
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(4)}
+                    onLoad={onload}
+                    src={img4}
+                    width="432"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(5)}
+                    src={img5}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+            </ImgRow>
+
+            <ImgRow>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(6)}
+                    src={img6}
+                    onLoad={onload}
+                    width="432"
+                    alt="jjl img1"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(7)}
+                    src={img7}
+                    onLoad={onload}
+                    width="192"
+                    alt="jjl img2"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(8)}
+                    src={img8}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+            </ImgRow>
+
+            <ImgRow>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(9)}
+                    src={img9}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(10)}
+                    src={img10}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(11)}
+                    src={img11}
+                    onLoad={onload}
+                    width="432"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(4)}
-                src={img4}
-                width="432"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(5)}
-                src={img5}
-                width="237"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(12)}
+                    src={img12}
+                    onLoad={onload}
+                    width="194"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(6)}
-                src={img6}
-                width="432"
-                alt="jjl img1"
-              />
-              <Img
-                onClick={() => handlePhotoClick(7)}
-                src={img7}
-                width="192"
-                alt="jjl img2"
-              />
-              <Img
-                onClick={() => handlePhotoClick(8)}
-                src={img8}
-                width="237"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(13)}
+                    src={img13}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(14)}
+                    src={img14}
+                    onLoad={onload}
+                    width="194"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(15)}
+                    src={img15}
+                    onLoad={onload}
+                    width="432"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(9)}
-                src={img9}
-                width="237"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(10)}
-                src={img10}
-                width="237"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(11)}
-                src={img11}
-                width="432"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(16)}
+                    src={img16}
+                    onLoad={onload}
+                    width="432"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(17)}
+                    src={img17}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(12)}
-                src={img12}
-                width="194"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(18)}
+                    src={img18}
+                    onLoad={onload}
+                    width="432"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(19)}
+                    src={img19}
+                    onLoad={onload}
+                    width="194"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(20)}
+                    src={img20}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(13)}
-                src={img13}
-                width="237"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(14)}
-                src={img14}
-                width="194"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(15)}
-                src={img15}
-                width="432"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(21)}
+                    src={img21}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(22)}
+                    src={img22}
+                    onLoad={onload}
+                    width="237"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(23)}
+                    src={img23}
+                    onLoad={onload}
+                    width="432"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
             <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(16)}
-                src={img16}
-                width="432"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(17)}
-                src={img17}
-                width="237"
-                alt="jjl img3"
-              />
-            </ImgRow>
-            <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(18)}
-                src={img18}
-                width="432"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(19)}
-                src={img19}
-                width="194"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(20)}
-                src={img20}
-                width="237"
-                alt="jjl img3"
-              />
-            </ImgRow>
-            <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(21)}
-                src={img21}
-                width="237"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(22)}
-                src={img22}
-                width="237"
-                alt="jjl img3"
-              />
-              <Img
-                onClick={() => handlePhotoClick(23)}
-                src={img23}
-                width="432"
-                alt="jjl img3"
-              />
-            </ImgRow>
-            <ImgRow>
-              <Img
-                onClick={() => handlePhotoClick(24)}
-                src={img24}
-                width="194"
-                alt="jjl img3"
-              />
+              <FadeIn
+                height={imageHeight}
+                duration={imageFadeInDuration}
+                easing={"ease-in-out"}
+              >
+                {onload => (
+                  <Img
+                    onClick={() => handlePhotoClick(24)}
+                    src={img24}
+                    onLoad={onload}
+                    width="194"
+                    alt="jjl img3"
+                  />
+                )}
+              </FadeIn>
             </ImgRow>
           </ImgContainer>
         </Container>
